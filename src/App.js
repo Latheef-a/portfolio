d3db4f965dd02c73f56d594e3a9f3ded76e8e5e8
Latheef-a <latheefee2004@gmail.com>
2026-03-26 18:04:58 +0530

    portfolio initial

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import dash3 from "./assets/Dashboard3.png";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="container">
+import { useState } from "react";
 
+function App() {
+const [selectedImage, setSelectedImage] = useState(null);
+
+  return (  
+    <div className="container">
+    
       {/* HERO */}
       <section className="hero">
         <h1>Hi, I'm Latheef 👋</h1>
         <h2>I build modern websites and web applications</h2>
         <p>Focused on creating clean, responsive and user-friendly designs.</p>
       </section>
+    
+        <section className="about">
+         <h2>About Me</h2>
+            <p>
+            I am passionate about building user-friendly web applications and continuously improving my skills.
+            </p>
+    </section>
+
 
       {/* SKILLS */}
       <section className="skills">
@@ -38,7 +50,8 @@ function App() {
           <p>Creative talent discovery platform built with React.</p>
           <a href="https://artistverse-creativa.vercel.app" target="_blank">
             Live Demo
-          </a>
+          </a> 
+          <a href="https://github.com/Latheef-a/artistverse-creativa.git" target="_blank">GitHub</a>
         </div>
 
         {/* Other Projects */}
@@ -48,6 +61,7 @@ function App() {
            <a href="https://youtube-clone-alpha-one-65.vercel.app" target="_blank">
             Live Demo
           </a>
+           <a href="https://github.com/Latheef-a/Youtube-Clone.git" target="_blank">GitHub</a>
         </div>
 
         <div className="project-card">
@@ -56,6 +70,7 @@ function App() {
            <a href="https://artistverse-ten.vercel.app" target="_blank">
             Live Demo
           </a>
+           <a href="https://github.com/Latheef-a/artistverse.git" target="_blank">GitHub</a>
         </div>
       </section>
 
@@ -65,10 +80,16 @@ function App() {
         <p>Interactive dashboards built using Power BI</p>
         <p>Foucsed on data visualization and insights using Power BI.</p>
 
-        <div className="Dashboard-container">
-          <img src={dash1} alt="dashboard" />
-           <img src={dash2} alt="dashboard" />
-            <img src={dash3} alt="dashboard" />
+        <div className="dashboard-container">
+          <img src={dash1} alt="dashboard" onClick={() => setSelectedImage(dash1)} />
+           <img src={dash2} alt="dashboard" onClick={() => setSelectedImage(dash2)}/>
+            <img src={dash3} alt="dashboard" onClick={() => setSelectedImage(dash3)}/>
+
+            {selectedImage && (
+  <div className="image-modal" onClick={() => setSelectedImage(null)}>
+    <img src={selectedImage} alt="large view" />
+  </div>
+)}
         </div>
       </section>
 
@@ -76,10 +97,17 @@ function App() {
       <section className="contact">
         <h2>Contact Me 📩</h2>
         <p>Email: latheefee2004@gmail.com</p>
+
+        <div className="contact-links">
+          <a href="https://github.com/" target="_blank">Github</a>
+          <a href="https://www.linkedin.com/in/latheef004/" target="_blank">LinkedIn</a>
+          <p>Open to interships and freelance opportunities</p>
+        </div>
       </section>
 
     </div>
   );
-}
 
+}
 export default App;
+    
